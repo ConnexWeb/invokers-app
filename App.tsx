a3ec1e2efabbfbba93860d372f16b5f732ddaf7a
cezarpm <cezar_pm@hotmail.com>
2023-10-12 03:08:49 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Cards } from "./src/screens/Material/Cards";
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Cards />
-    </View>
+    <ThemeProvider theme={THEME}>
+      <View style={styles.container}>
+        <Cards />
+      </View>
+    </ThemeProvider>
   );
 }
 
