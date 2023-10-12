798e7318015c1fadbc854f8ef58a7840a0ed59e4
cezarpm <cezar_pm@hotmail.com>
2023-10-12 03:05:02 -0300

    feat(Image): componente criado

---
+++ src/screens/Material/Cards/Image/index.tsx
@@ -0,0 +1,24 @@
+import Button from "../../../../components/Button";
+import { CardImagesChoice } from "../mockData";
+
+import * as S from "./styles";
+
+type Props = {
+  nextCard: () => void;
+} & CardImagesChoice;
+
+export const Image = ({ title, imageUrl, description, nextCard }: Props) => (
+  <S.Wrapper>
+    <S.Question>{title}</S.Question>
+
+    <S.Image resizeMode="contain" source={{ uri: imageUrl }} />
+
+    <S.Question>{description}</S.Question>
+
+    <S.WrapperButton>
+      <Button onPress={nextCard} type="callToAction" variant="checking">
+        Próximo
+      </Button>
+    </S.WrapperButton>
+  </S.Wrapper>
+);
---
+++ src/screens/Material/Cards/Image/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components/native";
+
+export const Wrapper = styled.View`
+  padding-top: 0px;
+  max-width: 650px;
+  align-self: center;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+export const WrapperButton = styled.View`
+  min-height: 110px;
+`;
+
+export const Image = styled.Image`
+  width: 300px;
+  height: 200px;
+  margin: 0 auto;
+`;
+
+export const Question = styled.Text`
+  display: flex;
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+  width: 300px;
+  margin: 10px auto;
+  color: #fff;
+`;
