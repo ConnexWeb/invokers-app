883a32a476ff8e54316b9445e5f558e4559c1aae
cezarpm <cezar_pm@hotmail.com>
2023-10-12 03:06:59 -0300

    @types

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import "styled-components";
+import { THEME } from "../styles/theme";
+
+declare module "styled-components" {
+  type ThemeType = typeof THEME;
+
+  export interface DefaultTheme extends ThemeType {}
+}
