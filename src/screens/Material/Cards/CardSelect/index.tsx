8263fcd88a6bd548f508825e4f5200a0653c3cab
cezarpm <cezar_pm@hotmail.com>
2023-10-12 03:06:28 -0300

    feat(CardSelect): componente criado

---
+++ src/screens/Material/Cards/CardSelect/index.tsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import * as S from "./styles";
+import { CardImagesChoice } from "../mockData";
+import Button from "../../../../components/Button";
+
+type Props = {
+  nextCard: () => void;
+} & CardImagesChoice;
+
+export const CardSelect = ({
+  answer,
+  imageUrl,
+  options,
+  isCorrects,
+  nextCard,
+}: Props) => {
+  const [cardOptions, setCardOptions] = useState([]);
+  const [optionsSelected, setOptionsSelected] = useState([]);
+  const [isValidate, setIsValidate] = useState(false);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const handleValidated = () => {
+    setIsValidate(true);
+    const isCorrectSorted = isCorrects.sort((a, b) => a - b);
+    const optionsSelectedSorted = optionsSelected.sort((a, b) => a - b);
+
+    console.log(
+      JSON.stringify(isCorrectSorted),
+      JSON.stringify(optionsSelectedSorted)
+    );
+
+    setIsCorrectAnswer(
+      JSON.stringify(isCorrectSorted) === JSON.stringify(optionsSelectedSorted)
+    );
+  };
+
+  const handlePress = (index: number) => {
+    if (isValidate) return;
+
+    setSelected(index);
+
+    if (optionsSelected.includes(index)) {
+      const actualOptionsSelected = optionsSelected.filter(
+        (option) => option !== index
+      );
+
+      const optionsMapped = cardOptions.map((option, i) => {
+        if (index === i) {
+          return {
+            ...option,
+            state: "UNSELECT",
+          };
+        }
+
+        return {
+          ...option,
+        };
+      });
+
+      setCardOptions(optionsMapped);
+      setOptionsSelected(actualOptionsSelected);
+
+      return;
+    }
+
+    const actualOptionsSelected = [...optionsSelected, index];
+
+    const optionsMapped = cardOptions.map((option, i) => {
+      if (index === i) {
+        return {
+          ...option,
+          state: "SELECTED",
+        };
+      }
+
+      return {
+        ...option,
+      };
+    });
+
+    setCardOptions(optionsMapped);
+    setOptionsSelected(actualOptionsSelected);
+  };
+
+  const handleNextCard = () => {
+    nextCard();
+    setIsValidate(false);
+    setSelected(null);
+    setOptionsSelected([]);
+  };
+
+  useEffect(() => {
+    const optionsMapped = options.map((option) => {
+      return {
+        answer: option,
+        state: "UNSELECT",
+      };
+    });
+
+    setCardOptions(optionsMapped);
+  }, []);
+
+  return (
+    <S.Wrapper>
+      <S.WrapperQuestions>
+        <S.Question>{answer}</S.Question>
+
+        {imageUrl && (
+          <S.Image resizeMode="contain" source={{ uri: imageUrl }} />
+        )}
+
+        <S.Questions>
+          {cardOptions.map((answer, index) => (
+            <>
+              <S.Button onPress={() => handlePress(index)} state={answer.state}>
+                <S.ButtonText>{answer.answer}</S.ButtonText>
+
+                {answer.state === "SELECTED" && <S.CheckIcon />}
+              </S.Button>
+            </>
+          ))}
+        </S.Questions>
+      </S.WrapperQuestions>
+
+      <S.WrapperButton>
+        {isValidate && (
+          <Button
+            onPress={handleNextCard}
+            type="callToAction"
+            variant={isCorrectAnswer ? "correct" : "incorrect"}
+          >
+            Próximo
+          </Button>
+        )}
+
+        {!isValidate && (
+          <>
+            {selected !== null && (
+              <Button
+                onPress={handleValidated}
+                type="callToAction"
+                variant="checking"
+              >
+                Validar
+              </Button>
+            )}
+
+            {selected === null && (
+              <Button type="callToAction" variant="disabled">
+                Validar
+              </Button>
+            )}
+          </>
+        )}
+      </S.WrapperButton>
+    </S.Wrapper>
+  );
+};
---
+++ src/screens/Material/Cards/CardSelect/styles.ts
@@ -0,0 +1,110 @@
+import styled, { css } from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+
+export const Wrapper = styled.View`
+  max-width: 650px;
+  align-self: center;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+export const WrapperQuestions = styled.View`
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Questions = styled.View``;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+export const Question = styled.Text`
+  display: flex;
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+  width: 300px;
+  margin: 10px auto;
+  color: white;
+`;
+
+export const WrapperButton = styled.View`
+  min-height: 110px;
+`;
+
+export const Image = styled.Image`
+  width: 200px;
+  height: 100px;
+  border-radius: 8px;
+  margin: 10px auto;
+`;
+
+export const CheckIcon = styled(AntDesign).attrs(({ theme }) => ({
+  name: "checkcircle",
+  size: 24,
+  color: "#8a2be2",
+}))`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  border-radius: 45px;
+  background-color: white;
+`;
+
+const ButtonModifiers = {
+  UNSELECT: (theme) => css`
+    border: 1px solid #e0e0e6;
+    border-bottom-color: #e0e0e6;
+    border-bottom-width: 4px;
+    border-bottom-style: solid;
+
+    background: ${theme.COLORS.GREY_500};
+
+    border: 1px solid ${theme.COLORS.GREY_700};
+
+    border-bottom-color: #6a51b2;
+    border-bottom-width: 4px;
+    border-bottom-style: solid;
+  `,
+  SELECTED: () => css`
+    background: rgba(138, 43, 226, 0.2);
+    border: 1px solid #8a2be2;
+    border-bottom-color: #6a51b2;
+    border-bottom-width: 4px;
+    border-bottom-style: solid;
+  `,
+};
+
+export const Button = styled.TouchableOpacity<{
+  state: "UNSELECT" | "SELECTED";
+}>`
+  position: relative;
+  border: 1px solid #e0e0e6;
+  box-shadow: 0px 3px 0px #e0e0e6;
+  height: 64px;
+  font-size: 17px;
+  margin-bottom: 16px;
+  width: 100%;
+  border-radius: 10px;
+  font-size: 1.1rem;
+  font-weight: 700;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ theme, state }) => css`
+    ${ButtonModifiers[state](theme)}
+  `}
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+`;
