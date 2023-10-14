99625211cc94812a1fc1d70cc4d8dcd2f94c4703
cezarpm <cezar_pm@hotmail.com>
2023-10-14 00:15:43 -0300

    fix(ProgressBar): melhoria visual

--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -3,14 +3,14 @@ import styled from "styled-components/native";
 import Animated from "react-native-reanimated";
 
 export const Track = styled.View`
-  height: 18px;
+  height: 14px;
   width: 100%;
-  border-radius: 3px;
+  border-radius: 5px;
   background-color: #505059;
 `;
 
 export const Progress = styled(Animated.View)`
-  height: 18px;
+  height: 14px;
   background-color: #00b37e;
   border-radius: 8px;
 `;
